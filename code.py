5a1c76b635b8e1e0a5d0065537d08b2d9d0d5744
BJ Chatterson <wjchat@umich.edu>
2017-01-27 13:29:30 -0500

    testing

--- code.py
+++ code.py
@@ -32,3 +32,10 @@ print("check_leap_year(2000): {}".format(check_leap_year(2000)))
 print("check_leap_year(1990): {}".format(check_leap_year(1990)))
 print("check_leap_year(2012): {}".format(check_leap_year(2012)))
 print("check_leap_year(2100): {}".format(check_leap_year(2100)))
+
+class UnitTests(unittests.TestCase):
+    def testFactorial(self):
+        assertEqual(factorial(4), )
+
+    def testLeapTrue(self):
+        self.assertTrue(check_leap_year(2004 == True))
